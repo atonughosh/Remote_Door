3eac1458168a947a172873eafcba2c975b2f7c25
ATONU GHOSH <atonughosh@outlook.com>
2017-05-11 20:26:29 +0530

    Update app.py

--- team6/app.py
+++ team6/app.py
@@ -1,1 +1,48 @@
+import RPi.GPIO as GPIO
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+GPIO.setmode(GPIO.BCM)
+pins = {
+   5 : {'name' : 'Pin 5', 'state' : GPIO.LOW}
+   }
+
+for pin in pins:
+   GPIO.setup(pin, GPIO.OUT)
+   GPIO.output(pin, GPIO.LOW)
+
+@app.route("/")
+def main():
+   for pin in pins:
+      pins[pin]['state'] = GPIO.input(pin)
+   templateData = {
+      'pins' : pins
+      }
+   return render_template('index.html', **templateData)
+
+@app.route("/<changePin>/<action>")
+def action(changePin, action):
+   changePin = int(changePin)
+   deviceName = pins[changePin]['name']
+   if action == "on":
+      GPIO.output(changePin, GPIO.HIGH)
+      message = "Turned " + deviceName + " on."
+   if action == "off":
+      GPIO.output(changePin, GPIO.LOW)
+      message = "Turned " + deviceName + " off."
+   if action == "toggle":
+      GPIO.output(changePin, not GPIO.input(changePin))
+      message = "Toggled " + deviceName + "."
+
+   for pin in pins:
+      pins[pin]['state'] = GPIO.input(pin)
+   templateData = {
+      'message' : message,
+      'pins' : pins
+   }
+
+   return render_template('index.html', **templateData)
+
+if __name__ == "__main__":
+   app.run(host='0.0.0.0', port=5000, debug=True)
 
