03829ecb4e7871f421464ae4fc5afca521782ca4
ATONU GHOSH <atonughosh@outlook.com>
2017-05-12 12:16:20 +0530

    Update keypad.py

--- team6/keypad.py
+++ team6/keypad.py
@@ -1,14 +1,15 @@
 import RPi.GPIO as GPIO
+import time
 
 GPIO.setmode(GPIO.BOARD)
 
 MATRIX = [ [1,2,3,'A'],
-		   [4,5,6,'B'],
-		   [7,8,9,'C'],
-		   ['*',0,'#,'D]]
+	   [4,5,6,'B'],
+	   [7,8,9,'C'],
+           ['*',0,'#','D'] ]
 
-ROW = [7,11,13,15]
-COL = [12,16,18,22]
+ROW = [11,12,13,15]
+COL = [37,35,33,31]
 
 for j in range(4):
 	GPIO.setup(COL[j], GPIO.OUT)
@@ -19,26 +20,19 @@ for i in range(4):
 
 try:
 	while(True):
-		pass
-	except KeyboardInterrupt:
-		GPIO.cleanup()
-
-GPIO.setup(COL[j], GPIO.OUT)
-GPIO.output(COL[j], 1)
-
-for i in range(4):
-	GPIO.setup(ROW[i], GPIO.IN, pull_up_down = GPIO.PUD_UP)
-
-try:
-	while(True):
 		for j in range(4):
-			GPIO.output(COL[j], 0)
+			GPIO.output(COL[j],0)
 
 			for i in range(4):
-				if GPIO.input(ROW[i] == 0):
+				if GPIO.input(ROW[i]) == 0:
+					time.sleep(0.2)
 					print(MATRIX[i][j])
+					time.sleep(0.2)
 					while(GPIO.input(ROW[i]) == 0):
+						time.sleep(0.2)
 						pass
+			
 			GPIO.output(COL[j],1)
-except KeyboardInterrupt
+
+except KeyboardInterrupt:
 	GPIO.cleanup()
