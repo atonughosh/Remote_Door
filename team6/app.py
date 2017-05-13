6c2f947b609a2a5daf59539df3cbc4ab173c799e
ATONU GHOSH <atonughosh@outlook.com>
2017-05-13 20:31:35 +0530

    Update app.py

--- team6/app.py
+++ team6/app.py
@@ -1,10 +1,12 @@
+#code to operate door lock through UI in a browser
+
 import RPi.GPIO as GPIO
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
 GPIO.setmode(GPIO.BCM)
 pins = {
-   5 : {'name' : 'Pin 5', 'state' : GPIO.LOW}
+   5 : {'name' : 'Pin 5', 'state' : GPIO.LOW}   #Pin 5 in BCM numbering is confured to operate the actuator
    }
 
 for pin in pins:
@@ -20,16 +22,17 @@ def main():
       }
    return render_template('index.html', **templateData)
 
+#code to turn pin 5 On and Off
 @app.route("/<changePin>/<action>")
 def action(changePin, action):
    changePin = int(changePin)
    deviceName = pins[changePin]['name']
    if action == "on":
       GPIO.output(changePin, GPIO.HIGH)
-      message = "Turned " + deviceName + " on."
+      message = "Turned " + deviceName + " on."       #Turn pin 5 On
    if action == "off":
       GPIO.output(changePin, GPIO.LOW)
-      message = "Turned " + deviceName + " off."
+      message = "Turned " + deviceName + " off."      #Turn pin 5 Off
    if action == "toggle":
       GPIO.output(changePin, not GPIO.input(changePin))
       message = "Toggled " + deviceName + "."
